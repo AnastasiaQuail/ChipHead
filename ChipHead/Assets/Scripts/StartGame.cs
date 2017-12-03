d4b8d9a5d2654fe2246bf3e791e43bbcf4a2ce5a
ilya1032 <34106815+ilya1032@users.noreply.github.com>
2017-12-03 16:00:08 +0300

    UI
    Добавил звуки при нажатии, изменил бэкграунд и размер текста

--- ChipHead/Assets/Scripts/StartGame.cs
+++ ChipHead/Assets/Scripts/StartGame.cs
@@ -5,9 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 
+    public AudioClip OnMouseEnterSound;
+
     public void LoadByIndex(int SceneIndex)
     {
         SceneManager.LoadScene(SceneIndex);
     }
-	
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); 
+#endif
+    }
+
+    public void OnMouseEnter()
+    {
+        AudioManager.instance.PlaySingle(OnMouseEnterSound);
+    }
+
 }
