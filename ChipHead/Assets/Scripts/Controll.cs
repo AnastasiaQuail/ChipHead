bb064778738b31f1036b3cbc36cef713865842f0
hipopasran <vladimirkeda@yandex.ru>
2017-12-21 03:42:43 +0300

    rope_alfa

--- ChipHead/Assets/Scripts/Controll.cs
+++ ChipHead/Assets/Scripts/Controll.cs
@@ -16,6 +16,10 @@ public class Controll : MonoBehaviour {
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
 
+    public bool swing = false;
+    int swingForce = 25;
+    bool canSwing = true;
+
     //bool pullAvaliable = false;
     //public LayerMask Pull;
     //public Transform pullCheck;
@@ -37,8 +41,10 @@ public class Controll : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-
+        if (swing == false)
+        {
+            grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        }
         float move = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
@@ -63,13 +69,48 @@ public class Controll : MonoBehaviour {
 
         if (grounded && ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) || Input.GetKeyDown(KeyCode.Joystick1Button0)))
         {
-            rb.AddForce(new Vector2(0, jumpForce));
+            if (swing == false)
+            {
+                rb.AddForce(new Vector2(0, jumpForce));
+            }
+            if(swing==true)
+            {
+                swing = false;
+                Destroy(GetComponent<HingeJoint2D>());
+                rb.AddForce(new Vector2(0, jumpForce));
+                
+                StartCoroutine(Wait());
+
+
+            }
         }
         if(rb.velocity.y < 0)
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
         
+        
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag=="Rope" && canSwing == true)
+        {
+            grounded = true;
+            canSwing = false;
+            swing = true;
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            HingeJoint2D hinge = gameObject.AddComponent<HingeJoint2D>() as HingeJoint2D;
+            hinge.connectedBody = other.gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.4f);
+        canSwing = true;
+        GetComponent<CapsuleCollider2D>().enabled = true;
     }
 
    
